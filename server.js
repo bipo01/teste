207bdef6a17738df5a3ebbcab1b41d046ef9ac46
João Bispo <jsbispo1101@gmail.com>
2024-04-09 10:47:10 -0400

    path public

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
