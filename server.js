c68d72e0c22ee339e193fcae624444a3e2a5a38f
João Bispo <jsbispo1101@gmail.com>
2024-04-09 10:32:56 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import ejs from "ejs";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server on port ${port}`);
+});
