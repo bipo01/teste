1392537fac19fb0fb415cfb66a7d41f0a2a07df6
João Bispo <jsbispo1101@gmail.com>
2024-04-09 10:44:25 -0400

    dirname

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from "express";
 import ejs from "ejs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
